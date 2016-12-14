c500eeac581f661a3b8715ba1b1ae2f8f5dc23d7
j42j <julia.sedona@gmail.com>
2016-12-13 22:46:53 -0500

    Finished blackjack functionality

--- blackjack.py
+++ blackjack.py
@@ -1,23 +1,51 @@
-"""Play a round of blackjack against the dealer"""
+"""Play multiple rounds of blackjack against the dealer"""
 import random
 import itertools
 
 class Card():
-    """put doc here"""
+    """Cards class has attributes suit (H,S,C,D)
+    and rank (1-13) where 11, 12, 13 is J, Q, K"""
+
+    ranks = range(1, 14)
+    suits = ('Hearts', 'Spades', 'Diamonds', 'Clubs')
+
     def __init__(self, suit, rank):
         self.suit = suit
         self.rank = rank
 
+    def get_pretty_rank(self):
+        pretty_rank = {x: y for x, y in zip(list(self.ranks),
+                       ["Ace", *list(range(2,11)), "Jack", "Queen", "King"])}
+        return str(pretty_rank[self.rank])
+
     def __repr__(self):
-        return str(self.rank) + self.suit
+        return self.get_pretty_rank() +  ' of ' + self.suit
 
     def get_points(self):
-        if (1 < self.rank <= 10):
+        """If card has rank 2-10, return rank
+        If card is J, Q, or K, return 10
+        If card is Ace, return 1 and 11 (list)"""
+
+        if 1 < self.rank <= 10:
             return [self.rank]
         elif (self.rank > 10):
             return [10]
 
         return [1, 11]
+
+
+class Deck(Card):
+    """Deck class extends Card"""
+
+    def __init__(self):
+        self.deck = []
+        for c_suit, c_rank in itertools.product(Card.suits, Card.ranks):
+            self.deck.append(Card(c_suit, c_rank))
+
+    def get_shuffled_deck(self):
+        random.shuffle(self.deck)
+        return self.deck
+
 
 class Hand():
     def __init__(self, c1, c2):
@@ -32,7 +60,7 @@ class Hand():
         points = [0]
         for c in self.cards:
             cp = c.get_points()
-            points = [x + y for x, y in itertools.product(points, cp)]
+            points = [x + y for x, y in itertools.product(points,  cp)]
 
         points.sort()
         best = points.pop()
@@ -45,48 +73,38 @@ class Hand():
     def __repr__(self):
         return repr(self.cards)
 
-ranks = range(1, 14)
-suits = ('H', 'S', 'D', 'C')
-deck = []
-for suit, rank in itertools.product(suits, ranks):
-    deck.append(Card(suit, rank))
-
-def get_shuffled_deck():
-    d = deck[:]
-    random.shuffle(d)
-    return d
 
 def main():
     while True:
         ans = input('Do you want to start a Blackjack game? (y/n):  ')
-        cleaned = ans.strip().lower()
-        if cleaned in ('n', 'no'):
+        if ans == 'n':
             break
-        elif cleaned not in ('y', 'yes'):
+        elif ans != 'y':
             print('Invalid response.')
             continue
 
-        game_deck = get_shuffled_deck()
-
+        game_deck = Deck().get_shuffled_deck()
         p_hand = Hand(game_deck.pop(), game_deck.pop())
         d_hand = Hand(game_deck.pop(), game_deck.pop())
 
         print("The dealer's face-up card is ", d_hand.cards[0])
+        print("Your hand is ", p_hand)
         p_points = p_hand.get_points()
         d_points = d_hand.get_points()
 
-        # if p_points == 21:
-        #     print("Your hand is ", p_hand)
-        #     if d_points != 21:
-        #         print("You win!")
-        #     else:
-        #         print("You tie!")
-        #     break
+        # Check for naturals
+        if p_points == 21:
+            print('The dealer\'s hand is ', d_hand)
+            if d_points != 21:
+                print('You have Blackjack! You win!')
+            else:
+                print('You tie!')
+        else:
+            if d_points == 21:
+                print('You lose!')
 
-
-        while p_points < 21:
-            print("Your hand is ", p_hand)
-
+        #play hand of blackjack until bust or stand
+        while p_points < 21 and d_points != 21:
             a2 = input('Do you want to hit? (y/n):  ')
             if a2 == 'n':
                 break
@@ -98,6 +116,25 @@ def main():
 
         if p_points > 21:
             print("You went bust! You lose.")
+        elif p_points == 21:
+            print("The dealer's hidden card is ", d_hand.cards[1])
+            print("The dealer's hand is ", d_hand)
+            print("You have Blackjack! You win!")
+        else:
+            print("The dealer's hidden card is ", d_hand.cards[1])
+            print("The dealer's hand is ", d_hand)
+            print("The dealer will play.")
+            while d_points < 17:
+                c = game_deck.pop()
+                d_hand.add_card(c)
+                d_points = d_hand.get_points()
+            print("The dealer's final hand is ", d_hand)
+            if d_points > 21 or p_points > d_points:
+                print("You win!")
+            elif p_points < d_points:
+                print("You lose!")
+            else:
+                print("You tie!")
 
 if __name__ == "__main__":
-main()
+    main()
